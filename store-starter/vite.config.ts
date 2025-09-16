ae8528a831bf70cc24a01f94f421a562cb05bef5
jannepeh <jannepeh@metropolia.fi>
2025-09-16 11:19:44 +0300

    first commit

---
+++ store-starter/vite.config.ts
@@ -0,0 +1,36 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "mediastore",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./contextHooks": "./src/hooks/contextHooks.tsx",
+        "./MediaContext": "./src/contexts/MediaContext.tsx",
+        "./UserContext": "./src/contexts/UserContext.tsx",
+        "./apiHooks": "./src/hooks/apiHooks.tsx",
+      },
+      shared: ["react", "react-dom", "react-router-dom"],
+    }),
+  ],
+  server: {
+    port: 3001, // Set the desired port here
+  },
+  preview: {
+    port: 3001, // Set the desired port here
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    target: "esnext",
+  },
+});
