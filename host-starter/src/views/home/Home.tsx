390c3847388d6e5f63adbb6c746dbee45b951b70
jannepeh <jannepeh@metropolia.fi>
2025-09-16 11:54:37 +0300

    refactor: remove TODO comments and implement mediaItems in Home component

--- host-starter/src/views/home/Home.tsx
+++ host-starter/src/views/home/Home.tsx
@@ -1,11 +1,10 @@
-// TODO: Import the Front component from the front_and_sidebar mfe
-// TODO: Import the useMediaContext hook from the mediastore mfe
+import { Front } from "front_and_sidebar/Front";
+import { useMediaContext } from "mediastore/contextHooks";
 
 const Home = () => {
-  // TODO: Use the useMediaContext hook to get the mediaItems
+  const { mediaItems } = useMediaContext();
 
-  // TODO: Pass the mediaItems to the Front component
-  return <div>Home</div>;
+  return <Front mediaItems={mediaItems} />;
 };
 
 export default Home;
